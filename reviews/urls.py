96580e2c0e36d39f0ff8ec048192b7ed00b695c6
Florent Verdu <verdu.florent@gmail.com>
2026-08-15 16:37:21 +0200

    Implement ticket CRUD

--- reviews/urls.py
+++ reviews/urls.py
@@ -5,4 +5,7 @@ from . import views
 
 urlpatterns = [
     path('feed/', views.feed, name='feed'),
+    path('tickets/create/', views.create_ticket, name='create_ticket'),
+    path('tickets/<int:ticket_id>/edit/', views.edit_ticket, name='edit_ticket'),
+    path('tickets/<int:ticket_id>/delete/', views.delete_ticket, name='delete_ticket'),
 ]
--- reviews/views.py
+++ reviews/views.py
@@ -1,7 +1,79 @@
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, redirect, render
+
+from .forms import TicketForm
+from .models import Ticket
 
 
 @login_required
 def feed(request):
     return render(request, 'reviews/feed.html')
+
+@login_required
+def create_ticket(request):
+    form = TicketForm()
+
+    if request.method == 'POST':
+        form = TicketForm(request.POST, request.FILES)
+
+        if form.is_valid():
+            ticket = form.save(commit=False)
+            ticket.user = request.user
+            ticket.save()
+
+            return redirect('feed')
+
+    return render(
+        request,
+        'reviews/create_ticket.html',
+        {'form': form},
+    )
+
+@login_required
+def feed(request):
+    tickets = Ticket.objects.all()
+
+    return render(
+        request,
+        'reviews/feed.html',
+        {'tickets': tickets},
+    )
+@login_required
+def edit_ticket(request, ticket_id):
+    ticket = get_object_or_404(
+    Ticket,
+    id=ticket_id,
+    user=request.user
+    )
+
+    form = TicketForm(instance=ticket)
+
+    if request.method == 'POST':
+        form = TicketForm(
+            request.POST,
+            request.FILES,
+            instance=ticket
+        )
+
+        if form.is_valid():
+            form.save()
+            return redirect('feed')
+
+    return render(
+        request,
+        'reviews/edit_ticket.html',
+        {'form': form},
+    )
+@login_required
+def delete_ticket(request, ticket_id):
+    ticket = get_object_or_404(Ticket, id=ticket_id)
+
+    if request.method == 'POST':
+        ticket.delete()
+        return redirect('feed')
+
+    return render(
+        request,
+        'reviews/delete_ticket.html',
+        {'ticket': ticket},
+    )
