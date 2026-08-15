6ff828acfc8194f70bb170ae58f6329cf70c1c61
Florent Verdu <verdu.florent@gmail.com>
2026-08-15 16:07:15 +0200

    Add review domain models

--- reviews/admin.py
+++ reviews/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Review, Ticket, UserFollows
+
+
+admin.site.register(Ticket)
+admin.site.register(Review)
+admin.site.register(UserFollows)
