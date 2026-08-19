0f6db2b163088016ddfd6167bc86cd1928ba954c
verduflorent <verdu.florent@gmail.com>
2026-08-20 00:03:09 +0200

    Fix Flake8 style issues

--- authentication/forms.py
+++ authentication/forms.py
@@ -1,12 +1,14 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
+
 from .models import User
+
 
 class LoginForm(forms.Form):
     username = forms.CharField(max_length=63)
     password = forms.CharField(
         max_length=63,
-        widget=forms.PasswordInput
+        widget=forms.PasswordInput,
     )
 
 
